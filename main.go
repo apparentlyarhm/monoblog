f6a3c5fc0410e7623f0dd46c3d71eeb09c82ca36
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2025-12-26 10:25:57 +0530

    Add middleware for logging and scrape thing

--- main.go
+++ main.go
@@ -5,12 +5,23 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+	"slices"
 )
 
 // This tells Go: "Take the 'dist' folder and bake it into this binary"
 //
 //go:embed dist/*
 var content embed.FS
+
+var BM = []string{
+	"/.env",
+	"/config/.env",
+	"/admin",
+	"/wp-admin",
+	"/login",
+	"/phpmyadmin",
+	"/id_rsa",
+}
 
 func main() {
 	distDir, err := fs.Sub(content, "dist")
@@ -20,7 +31,7 @@ func main() {
 
 	fileServer := http.FileServer(http.FS(distDir))
 
-	http.Handle("/", fileServer)
+	http.Handle("/", prankMW(loggerMW(fileServer)))
 
 	log.Println(`
 ⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⢕⠕⠕⠕⠕⢕⢕
@@ -44,4 +55,21 @@ Starting server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func loggerMW(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s\n", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func prankMW(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if slices.Contains(BM, r.URL.Path) {
+			http.Redirect(w, r, "https://www.youtube.com/watch?v=_Gn-2ip4kMw", http.StatusFound)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
