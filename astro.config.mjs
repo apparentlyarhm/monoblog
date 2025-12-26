5adba31724986afefcc05cb9be3cb9f2d041f663
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2025-12-26 22:11:24 +0530

    Add sitemap integration and update robots.txt for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        shikiConfig: {
-            theme: 'github-dark', // we will what all we can add later
-            wrap: true,
-        },
-    },
-    site: 'https://blog.arhm.dev', 
+  markdown: {
+      shikiConfig: {
+          theme: 'github-dark', // we will what all we can add later
+          wrap: true,
+      },
+  },
+
+  site: 'https://blog.arhm.dev',
+  integrations: [sitemap()],
 });
