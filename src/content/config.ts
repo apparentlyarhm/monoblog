ff84e566680ba3711d30a3bd7164dc1b8d06fee3
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2026-02-24 14:35:50 +0530

    refactor- add comments

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,10 +2,19 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
 	schema: z.object({
+		// Blog's title
 		title: z.string(),
+
+		// Date of the blog post
 		date: z.string(),
+
+		// Optional tags for the blog post
 		tags: z.string().optional(),
+
+		// Optional description for the blog post
 		description: z.string().optional(),
+
+		// Optional image for the blog post
 		image: z.string().optional(),
 	}),
 });
