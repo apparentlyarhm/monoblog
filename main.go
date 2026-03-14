62be3551b2d48f28e3e1b20b019881107ce09a57
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2026-03-14 11:26:37 +0530

    add un optimised handlers for view capture

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
 	"embed"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
 	"io/fs"
 	"log"
 	"mime"
-	"net"
+	"monoblog/server"
 	"net/http"
 	"os"
 	"runtime"
-	"slices"
-	"sync"
+	"time"
 
-	"golang.org/x/time/rate"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 )
 
 // This tells Go: "Take the 'dist' folder and bake it into this binary"
@@ -22,134 +29,22 @@ import (
 //go:embed all:dist
 var content embed.FS
 
-var (
-	visitors = make(map[string]*rate.Limiter)
-	mu       sync.Mutex
-)
-
-var BM = []string{
-	"/.env",
-	"/config/.env",
-	"/admin",
-	"/wp-admin",
-	"/login",
-	"/phpmyadmin",
-	"/id_rsa",
-	"/.git",
+type ViewPayload struct {
+	Slug     string `json:"slug"`
+	IssuedAt int64  `json:"iat"`
+	Nonce    string `json:"nonce"`
 }
 
-// ========== MIDDLEWARES ==========
-
-// Ok so there are a lot of middlewares now,
-// We will have to move them to a separate file at some point.
-
-func loggerMW(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// we will deliberatly see RemoteAddr value to see what cloudflare is sending
-
-		cf := true
-		a := r.Header.Get("CF-Connecting-IP")
-		if a == "" {
-			cf = false
-		}
-
-		// TODO: revert back to original after testing
-		log.Printf("%s %s rm: %s cf: %t\n", r.Method, r.URL.Path, r.RemoteAddr, cf)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func prankMW(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if slices.Contains(BM, r.URL.Path) {
-			http.Redirect(w, r, "https://www.youtube.com/watch?v=_Gn-2ip4kMw", http.StatusFound)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func custom404MW(next http.Handler, notFoundHTML []byte) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cw := &customResponseWriter{ResponseWriter: w, status: 200}
-		next.ServeHTTP(cw, r)
-
-		if cw.status == http.StatusNotFound {
-			w.Header().Set("Content-Type", "text/html; charset=utf-8")
-			w.WriteHeader(http.StatusNotFound)
-
-			w.Write(notFoundHTML)
-		}
-	})
-}
-
-func rateLimitMW(next http.Handler, rateLimitHTML []byte) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		limiter := getVisitor(getIP(r))
-
-		if !limiter.Allow() {
-			w.Header().Set("Content-Type", "text/html; charset=utf-8")
-			w.WriteHeader(http.StatusTooManyRequests)
-			w.Write(rateLimitHTML)
-
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-// ======= CUSTOM HELPERS OR SMTH ==========
-
-type customResponseWriter struct {
-	http.ResponseWriter
-	status int
-}
-
-func (w *customResponseWriter) WriteHeader(code int) {
-	w.status = code
-	if code != http.StatusNotFound {
-		w.ResponseWriter.WriteHeader(code)
-	}
-}
-
-func (w *customResponseWriter) Write(p []byte) (int, error) {
-	if w.status == http.StatusNotFound {
-		return len(p), nil
-	}
-
-	return w.ResponseWriter.Write(p)
-}
-
-func getVisitor(ip string) *rate.Limiter {
-	mu.Lock()
-	defer mu.Unlock()
-	limiter, exists := visitors[ip]
-
-	if !exists {
-		limiter = rate.NewLimiter(2, 5)
-		visitors[ip] = limiter
-	}
-
-	return limiter
-}
-
-func getIP(r *http.Request) string {
-	cfIP := r.Header.Get("CF-Connecting-IP")
-	if cfIP != "" {
-		return cfIP
-	}
-
-	ip, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err != nil {
-		return r.RemoteAddr
-	}
-	return ip
-}
-
-// ========== MAIN FUNCTION ==========
+var httpClient = &http.Client{Timeout: 5 * time.Second}
 
 func main() {
+	godotenv.Load()
+
+	cfg, err := server.Load()
+	if err != nil {
+		log.Fatalf("FATAL: could not load config: %v", err)
+	}
+
 	mime.AddExtensionType(".js", "application/javascript")
 	mime.AddExtensionType(".css", "text/css")
 
@@ -169,18 +64,74 @@ func main() {
 	}
 
 	fileServer := http.FileServer(http.FS(distDir))
-	finalHandler := prankMW(loggerMW(rateLimitMW(custom404MW(fileServer, HTML404), HTML429)))
+	fh := server.PrankMW(server.LoggerMW(server.RateLimitMW(server.Custom404MW(fileServer, HTML404), HTML429)))
 
 	tmpl := template.Must(template.ParseFS(distDir, "index.html"))
 
+	// this is weird handler inside handler situation but i didnt want it to be a "traditional" server app - but it is one now..
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		if r.URL.Path != "/" {
-			finalHandler.ServeHTTP(w, r)
+		if r.URL.Path == "/views/init" {
+			slug := r.URL.Query().Get("slug")
+
+			if slug == "" {
+				http.Error(w, "missing slug", http.StatusBadRequest)
+				return
+			}
+
+			payload := ViewPayload{
+				Slug:     slug,
+				IssuedAt: time.Now().Unix(),
+				Nonce:    uuid.New().String(),
+			}
+
+			jsonBytes, _ := json.Marshal(payload)
+			base64Payload := base64.StdEncoding.EncodeToString(jsonBytes)
+
+			signature := signData(base64Payload, cfg.HmacSecret)
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(map[string]string{
+				"payload":   base64Payload,
+				"signature": signature,
+			})
+
 			return
 		}
 
-		diagnostics := fmt.Sprintf(
+		if r.URL.Path == "/views/record" {
+			if cfg.ProxyHost == "" {
+				log.Println("skipping view recording...")
+				return
+			}
+
+			body, _ := io.ReadAll(r.Body)
+			url := cfg.ProxyHost + cfg.ViewEndpoint
+
+			proxyReq, _ := http.NewRequest("POST", url, bytes.NewBuffer(body))
+
+			proxyReq.Header.Set("X-API-KEY", cfg.GlobalApiKey)
+			proxyReq.Header.Set("Content-Type", "application/json")
+
+			resp, err := httpClient.Do(proxyReq)
+			if err != nil {
+				http.Error(w, "proxy offline", http.StatusBadGateway)
+				return
+			}
+			defer resp.Body.Close()
+
+			w.WriteHeader(resp.StatusCode)
+			io.Copy(w, resp.Body)
+
+			return
+		}
+
+		if r.URL.Path != "/" {
+			fh.ServeHTTP(w, r)
+			return
+		}
+
+		dStr := fmt.Sprintf(
 			"go:%s // %s/%s // goroutines:%d // svc:%s // region:%s // inst:%s // commit:%s",
 			runtime.Version(),
 			runtime.GOOS,
@@ -192,11 +143,7 @@ func main() {
 			os.Getenv("RENDER_GIT_COMMIT"),
 		)
 
-		data := struct {
-			Diagnostics string
-		}{
-			Diagnostics: diagnostics,
-		}
+		data := struct{ Diagnostics string }{Diagnostics: dStr}
 
 		tmpl.Execute(w, data)
 	})
@@ -220,7 +167,14 @@ func main() {
 Starting server
 	`)
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func signData(data string, secret string) string {
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(data))
+
+	return hex.EncodeToString(h.Sum(nil))
 }
