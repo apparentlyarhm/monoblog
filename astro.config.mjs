57830a277276435c47035e25ba5f713e4e96fba9
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2026-05-28 15:11:07 +0530

    add deps for latex + stying changes + rem package lock

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
       shikiConfig: {
           theme: 'houston', // we will what all we can add later
           wrap: true,
